5563c57fc9d5ece6edcf945d9e790c0bff354dc0
naizo10 <naizo018767@gmail.com>
2024-03-01 02:50:06 +0900

    Add useLocked function to VotingEscrow hook

---
+++ hooks/VotingEscrow/useLocked.ts
@@ -0,0 +1,29 @@
+import { useReadContract } from "wagmi";
+import { useContractContext } from "lib/contexts/ContractContext";
+
+export default function useLocked(
+  address?: `0x${string}`,
+): ReturnType<
+  typeof useReadContract<readonly unknown[], "locked", readonly unknown[]>
+> {
+  const { addresses, abis } = useContractContext();
+
+  const config = {
+    address: addresses.VotingEscrow as `0x${string}`,
+    abi: abis.VotingEscrow,
+  };
+  const readFn = useReadContract<
+    readonly unknown[],
+    "locked",
+    readonly unknown[]
+  >({
+    ...config,
+    functionName: "locked",
+    args: [address],
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  return readFn;
+}
