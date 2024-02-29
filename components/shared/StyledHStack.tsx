a18434d8b5ab1a8c8b3352e9605188ba9a1fc816
naizo10 <naizo018767@gmail.com>
2024-03-01 02:49:38 +0900

    Refactor StyledHStack component in Chakra UI

--- components/shared/StyledHStack.tsx
+++ components/shared/StyledHStack.tsx
@@ -1,13 +1,16 @@
 import { HStack, StackProps, chakra } from "@chakra-ui/react";
 
-const StyledHStack: React.FC<
-  StackProps & { title: string; children: string; unit: string }
-> = ({ title, children, unit, ...props }) => {
+const StyledHStack: React.FC<StackProps & { title: string; unit: string }> = ({
+  title,
+  children,
+  unit,
+  ...props
+}) => {
   return (
     <HStack justifyContent={"space-between"} {...props}>
       <chakra.p>{title}</chakra.p>
       <chakra.p fontSize={"2xl"}>
-        <>{children}</>
+        {children}
         <chakra.span fontSize={"lg"} ml={1}>
           {unit}
         </chakra.span>
