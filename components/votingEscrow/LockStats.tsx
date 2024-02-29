a18d3ce81bb20f98c34cda052a2a18a5dd323ed2
naizo10 <naizo018767@gmail.com>
2024-03-01 02:50:39 +0900

    Add balance and lock status in LockStats component

--- components/votingEscrow/LockStats.tsx
+++ components/votingEscrow/LockStats.tsx
@@ -1,24 +1,45 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Spinner } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import { format } from "date-fns";
+import { tokenAmountFormat } from "lib/utils";
 import NewLockForm from "./NewLockForm";
 import IncreaseUnlockTimeForm from "./IncreaseUnlockTimeForm";
 import IncreaseAmountForm from "./IncreaseAmountForm";
 import WithdrawButton from "./WithdrawButton";
 import StyledHStack from "components/shared/StyledHStack";
+import useBalanceOf from "hooks/VotingEscrow/useBalanceOf";
+import useLocked from "hooks/VotingEscrow/useLocked";
 
 export default function LockStats({ address }: { address?: `0x${string}` }) {
   const { t } = useTranslation();
-
+  const { data: balance } = useBalanceOf(address) as {
+    data: bigint | undefined;
+  };
+  const { data: locked } = useLocked(address) as { data: bigint[] | undefined };
   return (
     <>
       <StyledHStack title={t("BALANCE")} unit={"veYMT"}>
-        {"0.0"}
+        {typeof balance === "undefined" ? (
+          <Spinner />
+        ) : (
+          <>{tokenAmountFormat(balance, 18, 2)}</>
+        )}
       </StyledHStack>
       <StyledHStack title={t("YMT_LOCKED")} unit={"YMT"} mt={1}>
-        {"0.0"}
+        {typeof locked === "undefined" ? (
+          <Spinner />
+        ) : (
+          <>{tokenAmountFormat(locked[0], 18, 2)}</>
+        )}
       </StyledHStack>
       <StyledHStack title={t("LOCKED_UNTIL")} unit={""} mt={1}>
-        {"-- / -- / --"}
+        {typeof locked === "undefined" ? (
+          <Spinner />
+        ) : locked[1] === BigInt(0) ? (
+          <>{"-- / -- / --"}</>
+        ) : (
+          <>{format(new Date(Number(locked[1]) * 1000), "yyyy / MM / dd")}</>
+        )}
       </StyledHStack>
 
       <HStack spacing={4} justifyContent={"flex-end"} mt={2}>
