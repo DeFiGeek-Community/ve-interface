9118f01b66e0aa3df0aa8389178ba252147f543b
naizo10 <naizo018767@gmail.com>
2024-03-08 15:51:49 +0900

    Add useClaimRewards hook in Vesting module

---
+++ hooks/Vesting/useClaimRewards.ts
@@ -0,0 +1,68 @@
+import { useEffect } from "react";
+import {
+  useSimulateContract,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+} from "wagmi";
+import { useContractContext } from "lib/contexts/ContractContext";
+
+export default function useClaimRewards({
+  address,
+  onSuccessWrite,
+  onError,
+  onSuccessConfirm,
+}: {
+  address?: `0x${string}`;
+  onSuccessWrite?: (data: any) => void;
+  onError?: (error: Error) => void;
+  onSuccessConfirm?: (data: any) => void;
+}): {
+  prepareFn: ReturnType<typeof useSimulateContract>;
+  writeFn: ReturnType<typeof useWriteContract>;
+  waitFn: ReturnType<typeof useWaitForTransactionReceipt>;
+} {
+  const { addresses, abis } = useContractContext();
+
+  const prepareFn = useSimulateContract({
+    address: addresses.Vesting as `0x${string}`,
+    abi: abis.Vesting,
+    functionName: "claimV1RetroactiveRewards",
+    args: [],
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  const writeFn = useWriteContract({
+    mutation: {
+      onSuccess(data) {
+        onSuccessWrite && onSuccessWrite(data);
+      },
+      onError(e: Error) {
+        onError && onError(e);
+      },
+    },
+  });
+
+  const waitFn = useWaitForTransactionReceipt({
+    hash: writeFn?.data,
+  });
+
+  useEffect(() => {
+    // 成功時
+    if (waitFn.isSuccess) {
+      onSuccessConfirm && onSuccessConfirm(waitFn.data);
+    }
+
+    // エラー時
+    if (waitFn.isError) {
+      onError && onError(waitFn.error);
+    }
+  }, [waitFn, onSuccessConfirm, onError]);
+
+  return {
+    prepareFn,
+    writeFn,
+    waitFn,
+  };
+}
