7a37bce53fb84fcae1b20163dd131911261272e6
naizo10 <naizo018767@gmail.com>
2024-03-14 03:11:26 +0900

    Add getChain function to utils/chain.ts

---
+++ lib/utils/chain.ts
@@ -0,0 +1,12 @@
+import * as chains from "viem/chains";
+
+const getChain = (chainId: number): chains.Chain => {
+  for (const chain of Object.values(chains)) {
+    if (chain.id === chainId) {
+      return chain;
+    }
+  }
+  return chains.localhost;
+};
+
+export default getChain;
