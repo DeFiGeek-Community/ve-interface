b4d1ea2d0a7839089f17f8a505f9c5532585ede9
naizo10 <naizo018767@gmail.com>
2024-03-27 01:01:08 +0900

    Add useIntegrateFraction hook in Gauge module

---
+++ hooks/Gauge/useIntegrateFraction.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useReadContract, useAccount } from "wagmi";
+import { useContractContext } from "lib/contexts/ContractContext";
+
+export default function useIntegrateFraction(
+  address?: `0x${string}`,
+): ReturnType<typeof useReadContract> {
+  const { addresses, abis, refetchFlag } = useContractContext();
+  const { address: accentAddress } = useAccount();
+  const config = {
+    address: addresses.Gauge as `0x${string}`,
+    abi: abis.Gauge,
+  };
+  const readFn = useReadContract({
+    ...config,
+    functionName: "integrateFraction",
+    args: [address || "0x0"],
+    query: {
+      enabled: !!accentAddress,
+    },
+  });
+
+  useEffect(() => {
+    readFn.refetch();
+  }, [refetchFlag]);
+
+  return readFn;
+}
