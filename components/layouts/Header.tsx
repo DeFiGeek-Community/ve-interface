3a12b87e3354f98c706828a508d72f90024a8648
naizo10 <naizo018767@gmail.com>
2024-03-27 01:28:39 +0900

    Add responsive header menu for small screens

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -5,13 +5,20 @@ import {
   Link,
   HStack,
   useMediaQuery,
+  Text,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { useAccount } from "wagmi";
 import SvgYamatoLogWithTitle from "../svgs/YamatoLogo";
 
 export default function Header() {
   const { chain } = useAccount();
-  const [isLessThan520px] = useMediaQuery("(max-width: 520px)");
+  const [isLessThan700px] = useMediaQuery("(max-width: 700px)");
 
   return (
     <Box
@@ -29,13 +36,48 @@ export default function Header() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <HStack>
-            {!isLessThan520px && (
+          {isLessThan700px ? (
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList bg="#fcfaf2">
+                <MenuItem as={Link} href="https://app.yamato.fi/#/" >
+                  HOME
+                </MenuItem>
+                <MenuItem
+                  as={Link}
+                  href="/"
+                  style={{ pointerEvents: "none", opacity: 0.6 }}
+                >
+                  veYMT
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          ) : (
+            <HStack spacing={6}>
               <Link href="/">
                 <SvgYamatoLogWithTitle width={200} height={30} />
               </Link>
-            )}
-          </HStack>
+              <Link
+                href="https://app.yamato.fi/#/"
+                _hover={{ textDecoration: "none" }}
+                ml={4}
+              >
+                <Text fontWeight="bold">HOME</Text>
+              </Link>
+              <Link
+                href="/"
+                _hover={{ textDecoration: "none" }}
+                style={{ pointerEvents: "none", opacity: 0.6 }}
+              >
+                <Text fontWeight="bold">veYMT</Text>
+              </Link>
+            </HStack>
+          )}
           <HStack>
             {chain && <w3m-network-button />}
             <w3m-button balance={"hide"} />
