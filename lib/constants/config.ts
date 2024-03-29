ec8ad7c3cb9d1b554b01ae09ef84d053e56f010b
naizo10 <naizo018767@gmail.com>
2024-03-29 22:35:18 +0900

    Refactor yamato config to reduce code duplication

--- lib/constants/config.ts
+++ lib/constants/config.ts
@@ -18,33 +18,30 @@ export interface IEnvironmentConfig {
   };
 }
 
+const yamatoConfig: IConfig = {
+  TokenStartTimestamp: 0,
+  TokenDecimals: 18,
+  VestingPeriod: 31536000,
+  tokenName: "YMT",
+  veTokenName: "veYMT",
+  initialReward: true,
+  minterReward: true,
+  feeReward: true,
+  checkpoint: true,
+  homeUrl: "https://yamato-interface.vercel.app/#/",
+  tokenRewardsDocumentationUrl:
+    "https://docs.yamato.fi/ymt-yamato-dao-token/toveymtniyorufmingubsuto",
+};
+
 export const environmentConfig: IEnvironmentConfig = {
   yamato: {
     1: {
+      ...yamatoConfig,
       TokenStartTimestamp: 0,
-      TokenDecimals: 18,
-      VestingPeriod: 31536000,
-      tokenName: "YMT",
-      veTokenName: "veYMT",
-      initialReward: true,
-      minterReward: true,
-      feeReward: true,
-      checkpoint: true,
-      homeUrl: "",
-      tokenRewardsDocumentationUrl: "https://docs.yamato.fi/ymt-yamato-dao-token/toveymtniyorufmingubsuto",
     },
     11155111: {
+      ...yamatoConfig,
       TokenStartTimestamp: 1710832176,
-      TokenDecimals: 18,
-      VestingPeriod: 31536000,
-      tokenName: "YMT",
-      veTokenName: "veYMT",
-      initialReward: true,
-      minterReward: true,
-      feeReward: true,
-      checkpoint: true,
-      homeUrl: "https://yamato-interface.vercel.app/#/",
-      tokenRewardsDocumentationUrl: "https://docs.yamato.fi/ymt-yamato-dao-token/toveymtniyorufmingubsuto",
     },
   },
 };
