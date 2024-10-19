5d5eb716449986ffbad5ac8ac5d6283099447917
naizo10 <naizo018767@gmail.com>
2024-10-19 14:39:26 +0900

    Add new configuration for txjp token and update yamato config

--- lib/constants/config.ts
+++ lib/constants/config.ts
@@ -2,12 +2,15 @@ export interface IConfig {
   TokenStartTimestamp: number;
   TokenDecimals: number;
   VestingPeriod: number;
+  path: string;
   tokenName: string;
   veTokenName: string;
   initialReward: boolean;
   minterReward: boolean;
   feeReward: boolean;
+  multiTokenFeeDistributor: boolean;
   checkpoint: boolean;
+  rewardTokens: { name: string; address?: `0x${string}` }[];
   homeUrl?: string;
   tokenRewardsDocumentUrl?: string;
   veDocumentUrl?: string;
@@ -34,12 +37,15 @@ const yamatoConfig: IConfig = {
   TokenStartTimestamp: 0,
   TokenDecimals: 18,
   VestingPeriod: 31536000,
+  path: "/yamato",
   tokenName: "YMT",
   veTokenName: "veYMT",
   initialReward: true,
   minterReward: true,
   feeReward: true,
+  multiTokenFeeDistributor: false,
   checkpoint: true,
+  rewardTokens: [{ name: "ETH" }],
   homeUrl: "https://yamato-interface.vercel.app/#/",
   tokenRewardsDocumentUrl:
     "https://docs.yamato.fi/ymt-yamato-dao-token/toveymtniyorufmingubsuto",
@@ -57,6 +63,38 @@ const yamatoConfig: IConfig = {
   backgroundImagePath: "background_green.webp",
 };
 
+const txjpConfig: IConfig = {
+  TokenStartTimestamp: 0,
+  TokenDecimals: 8,
+  VestingPeriod: 0,
+  path: "/txjp",
+  tokenName: "TXJP",
+  veTokenName: "veTXJP",
+  initialReward: false,
+  minterReward: false,
+  feeReward: true,
+  multiTokenFeeDistributor: true,
+  checkpoint: false,
+  rewardTokens: [
+    { name: "TXJP", address: "0xdca6BcCecd7C25C654DFD80EcF7c63731B12Df5e" },
+    { name: "CJPY", address: "0xDED5F78d4fB19e935eb45dFf9912eB132F046782" },
+  ],
+  homeUrl: "https://defigeek.xyz/",
+  tokenRewardsDocumentUrl: "https://defigeek.xyz/txjp/",
+  veDocumentUrl: "https://defigeek.xyz/txjp/",
+  themeColors: {
+    backgroundColor: "#FFF", // 白
+    primaryColor: "#C51B41", // 濃い朱色
+    secondaryColor: "#FF7777", // 赤系
+    secondaryLightColor: "#FCD2d2", //
+    primaryText: "#818181", // ダークグレー
+    buttonText: "#FFF", // 白
+  },
+  projectLogoPath: "txjp/DFGC_logo_lockup.webp",
+  tokenLogoPath: "txjp/TXJP.png",
+  backgroundImagePath: "background/Group_200.webp",
+};
+
 export const environmentConfig: IEnvironmentConfig = {
   yamato: {
     1: {
@@ -66,6 +104,14 @@ export const environmentConfig: IEnvironmentConfig = {
     11155111: {
       ...yamatoConfig,
       TokenStartTimestamp: 1710832176,
+    },
+  },
+  txjp: {
+    1: {
+      ...txjpConfig,
+    },
+    11155111: {
+      ...txjpConfig,
     },
   },
 };
