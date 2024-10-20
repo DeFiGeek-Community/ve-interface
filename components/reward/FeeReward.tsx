a6300406558c44ad311b0b9b49e333adadeff2c6
naizo10 <naizo018767@gmail.com>
2024-10-20 15:18:25 +0900

    Refactor FeeReward component to use TokenReward

--- components/reward/FeeReward.tsx
+++ components/reward/FeeReward.tsx
@@ -1,34 +1,14 @@
-import { HStack, Box, chakra, Text } from "@chakra-ui/react";
+import { HStack, Text, VStack } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { useContractContext } from "lib/contexts/ContractContext";
-import StyledButton from "components/shared/StyledButton";
-import AmountRenderer from "components/shared/AmountRenderer";
 import { QuestionIcon } from "@chakra-ui/icons";
-import useClaim, { UseClaimReturn } from "hooks/FeeDistributor/useClaim";
-import useToastNotifications from "hooks/useToastNotifications";
+import TokenReward from "./TokenReward";
 import StyledTooltip from "components/shared/StyledTooltip";
 
 export default function FeeReward({ address }: { address?: `0x${string}` }) {
   const { t } = useTranslation();
-  const { config, triggerRefetch } = useContractContext();
-  const { tokenName, veTokenName } = config;
-  const { showSuccessToast, showErrorToast, showConfirmationToast } =
-    useToastNotifications();
-  const { prepareFn, writeFn, waitFn, writeContract } = useClaim({
-    callbacks: {
-      onSuccessWrite(data) {
-        showSuccessToast(data);
-      },
-      onError(e) {
-        showErrorToast(e.message);
-      },
-      onSuccessConfirm(data) {
-        showConfirmationToast();
-        triggerRefetch();
-      },
-    },
-  }) as UseClaimReturn;
-  const result = prepareFn.data?.result;
+  const { config } = useContractContext();
+  const { tokenName, veTokenName, rewardTokens } = config; 
 
   return (
     <HStack justifyContent={"space-between"} alignItems={"baseline"} mt={4}>
@@ -40,23 +20,11 @@ export default function FeeReward({ address }: { address?: `0x${string}` }) {
           <QuestionIcon fontSize={"md"} mb={1} ml={1} />
         </StyledTooltip>
       </Text>
-      <HStack spacing={2}>
-        <Box fontSize={"2xl"}>
-          <AmountRenderer amount={result} precision={6} />
-          <chakra.span fontSize={"lg"} ml={1}>
-            {"ETH"}
-          </chakra.span>
-        </Box>
-        <StyledButton
-          variant={"solid"}
-          size={"sm"}
-          isDisabled={!result || !writeFn.writeContract}
-          isLoading={writeFn.isPending || waitFn.isLoading}
-          onClick={() => writeContract()}
-        >
-          {t("CLAIM")}
-        </StyledButton>
-      </HStack>
+      <VStack spacing={2} align="start">
+        {rewardTokens.map(({ name, address }) => (
+          <TokenReward key={name} token={name} tokenAddress={address} />
+        ))}
+      </VStack>
     </HStack>
   );
 }
