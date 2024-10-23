834792835229c0b6df52d5c081c3cc3dcf2a822b
naizo10 <naizo018767@gmail.com>
2024-10-23 17:32:14 +0900

    Add loading spinner to AccountDashboard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import AccountDashboard from "components";
 import { ContractProvider } from "components/providers/ContractProvider";
+import { Spinner, Center } from "@chakra-ui/react";
 
 export default function VotingEscrow() {
   return (
     <ContractProvider systemName={"yamato"}>
-      <AccountDashboard />
+      {(isValid) => (
+        isValid ? (
+          <AccountDashboard />
+        ) : (
+          <Center height="100vh">
+            <Spinner size="xl" color="blue.500" />
+          </Center>
+        )
+      )}
     </ContractProvider>
   );
 }
