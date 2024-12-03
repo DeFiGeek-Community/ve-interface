bcc8af9d164616fada6a4ddf93672756820a2b87
naizo10 <naizo018767@gmail.com>
2024-12-03 14:36:13 +0900

    Add useGetTotalWeight hook in ScoreWeightController

---
+++ hooks/ScoreWeightController/useGetTotalWeight.ts
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useReadContract } from "wagmi";
+import { useContractContext } from "lib/contexts/ContractContext";
+
+export default function useGetTotalWeight(
+  address?: `0x${string}`,
+): ReturnType<
+  typeof useReadContract<
+    readonly unknown[],
+    "getTotalWeight",
+    readonly unknown[]
+  >
+> {
+  const { addresses, abis, refetchFlag } = useContractContext();
+
+  const config = {
+    address: addresses.GaugeController as `0x${string}`,
+    abi: abis.GaugeController,
+  };
+  const readFn = useReadContract<
+    readonly unknown[],
+    "getTotalWeight",
+    readonly unknown[]
+  >({
+    ...config,
+    functionName: "getTotalWeight",
+    args: [],
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  useEffect(() => {
+    readFn.refetch();
+  }, [refetchFlag]);
+
+  return readFn;
+}
